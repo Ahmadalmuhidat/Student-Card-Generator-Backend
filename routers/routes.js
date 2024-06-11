51e2d6449123dce7e5d9f06074e1d68240289665
Ahmadalmuhidat <ahmad.almuhidat@gmail.com>
2024-06-11 14:18:25 +0300

    added landing route

--- routers/routes.js
+++ routers/routes.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const generator = require("../modals/card")
 
-router.post('/', (req, res) => {
+router.post('/generate', (req, res) => {
   generator.generateCard(req, res);
+});
+
+router.get('/', (req, res) => {
+  res.send("hello world");
 });
 
 module.exports = router;
